6c8c1737226329c3537d28435ffeed6712c55ead
Sadikh-SA <abougueye96@yahoo.fr>
2019-07-01 09:56:21 +0000

    Avant Fin

--- Testeur/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include '../Classes/ServiceEtudiant.php';
-$test = new Service();	
-
-$matricule="SA911123";
-$etudiant = new Loger($matricule,"Camara","Babs","774625521","babs.Camara@gmail.com","1992-07-14","demi-pension","25","Campus A");
-$test->Supprimer($etudiant,$matricule);
-?>
